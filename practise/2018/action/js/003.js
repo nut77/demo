8f9f71f2e5f6dd5a41101651ded88a79c7298244
sunnynut <sunnynut77@gmail.com>
2018-12-27 18:51:25 +0800

    update

--- practise/2018/action/js/003.js
+++ practise/2018/action/js/003.js
@@ -1,6 +1,7 @@
 // 用到的轮播图的相关模块
 const Selector = {
     BOX: '.carousel',
+    INNER: '.carousel-inner',
     ACTIVE: '.active',
     ACTIVE_ITEM: '.active.carousel-item',
     ITEM: '.carousel-item',
@@ -29,4 +30,42 @@ $(Selector.INDICATORS).on('mouseover', 'li', function () {
     let curIndex = this.getAttribute('data-index');
 });
 
-// 轮播
+// 获取指定item的序号  从0开始
+function getElementIndex(element) {
+
+    let items = element && element.parentNode
+        ? [].slice.call(element.parentNode.querySelectorAll(Selector.ITEM))
+        : [];
+    return items.indexOf(element);
+}
+
+// 下一页
+$(".carousel-ctrlNext").on('click', function () {
+
+    $(Selector.ACTIVE_ITEM).removeClass('active').next(Selector.ITEM).addClass('active');
+    slide($(Selector.INNER)[0].offsetLeft, -getElementIndex($(Selector.ACTIVE_ITEM)[0]) * Box.WIDTH);
+});
+
+// 滚动到指定的轮播图 图片序号
+function slide(cur, target) {
+
+    // 装轮播图片的盒子
+    let $inner = $(Selector.INNER);
+
+    // 每次移动的距离
+    let step = cur - target > 0 ? 10 : -10;
+
+    // 切换轮播图的显示
+    let timer = setInterval(() => {
+
+        cur -= step;
+        if (Math.abs(cur - target) > Math.abs(step)) {
+
+            $inner.css('left', cur);
+        } else {
+
+            $inner.css('left', target);
+            clearInterval(timer);
+        }
+    }, 10);
+}
